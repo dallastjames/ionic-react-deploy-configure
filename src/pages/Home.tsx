2c8562bb90f27aebaf51b379eef40bfe37bf3764
Dallas James <dallastjames@gmail.com>
2021-05-11 13:53:45 -0600

    init to win it

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,26 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import { Deploy } from "cordova-plugin-ionic";
+import "./Home.css";
 
 const Home: React.FC = () => {
+  const configure = async (config: {
+    appId: string;
+    channel: string;
+  }): Promise<void> => {
+    console.log("configure!");
+    console.log("before the great change", await Deploy.getConfiguration());
+    await Deploy.configure(config);
+    console.log("after the great change", await Deploy.getConfiguration());
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,6 +34,26 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonButton
+          onClick={() =>
+            configure({
+              appId: "2a4cc4bf",
+              channel: "Test",
+            })
+          }
+        >
+          CONFIGURE 1
+        </IonButton>
+        <IonButton
+          onClick={() =>
+            configure({
+              appId: "34e34455",
+              channel: "Oh Yeah!",
+            })
+          }
+        >
+          CONFIGURE 2
+        </IonButton>
         <ExploreContainer />
       </IonContent>
     </IonPage>
